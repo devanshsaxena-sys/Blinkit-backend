c3bcbca77709f3c6aab98a612a7babf7e6617ade
devanshsaxena-sys <dhruvsaxena137@gmail.com>
2026-06-24 11:51:18 +0530

    Update DB config for Railway MySQL

--- Config/db.js
+++ Config/db.js
@@ -1,17 +1,23 @@
-const {Sequelize}=require('sequelize');
+const { Sequelize } = require('sequelize');
 
-const sequelize=new Sequelize('blinkit_db','root','root',{
-    host:'localhost',
-    dialect:'mysql'
-
-});
+const sequelize = new Sequelize(
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
+  {
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    dialect: 'mysql',
+    logging: false,
+  }
+);
 
 sequelize.authenticate()
-.then(()=>{
+  .then(() => {
     console.log('Connection has been established successfully');
-})
-.catch(err=>{
-    console.error('Unable to connect to the database:',err);
-});
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
-module.exports={sequelize};
+module.exports = { sequelize };
